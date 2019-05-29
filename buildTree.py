d2eefdeae4dc4d5a8cd725746c1ff78a88d11e6d
Andre Andrade <andreanndrade10@gmail.com>
2019-05-29 18:54:25 -0300

    nRound function

--- buildTree.py
+++ buildTree.py
@@ -6,17 +6,34 @@ def firstRound(vector):
     b = vector.pop()
     vector.append(a+b)
     firstRoot = Node(a+b, b, a)
-    return vector, root
+    return vector, firstRoot
 
 
 
 def nRound(vector, root):
+    tempRoot = root
     a = vector.pop()
     b = vector.pop()
+
     if a+b != 1:
         vector.append(a+b)
         rootn = Node(a+b)
-        rootn.right = root
+        rootn.right = tempRoot
         rootn.left = Node(b)
         nRound(vector, rootn)
+    rootn = Node(a+b)
+    rootn.right = tempRoot
+    rootn.left = Node(b)
     return rootn
+
+root = Node(.2)
+root.right = Node(.1)
+root.left = Node(.1)
+vector = [.3,.2,.2,.1,.2]
+
+
+print(nRound(vector, root))
+
+
+
+ 
