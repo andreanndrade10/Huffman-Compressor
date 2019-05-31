bdddb8e17d4c643d5e2fcc34b41af517464aa8ee
Andre Andrade <andreanndrade10@gmail.com>
2019-05-31 10:22:32 -0300

    some test updates

--- buildTree.py
+++ buildTree.py
@@ -1,4 +1,4 @@
-from binarytree import tree, bst, heap, build, Node
+from binarytree import build, Node
 
 
 def firstRound(vector):
@@ -14,24 +14,26 @@ def nRound(vector, root):
     tempRoot = root
     a = vector.pop()
     b = vector.pop()
-
-    if a+b < 1:
+    if  a+b < 1.0:
         vector.append(a+b)
         rootn = Node(a+b)
         rootn.right = tempRoot
         rootn.left = Node(b)
-        nRound(vector, rootn)    
-    rootn = Node(a+b)
-    rootn.right = tempRoot
-    rootn.left = Node(b)
-    return rootn
-
-root = Node(.2)
-root.right = Node(.1)
-root.left = Node(.1)
-vector = [.3,.2,.2,.1,.2]
-print(nRound(vector, root))
-
+        nRound(vector, rootn)
+        #print(rootn)
+    else:    
+        rootn = Node(a+b)
+        rootn.right = tempRoot
+        rootn.left = Node(b) 
+        print(rootn)   
+        return rootn
+    return rootn  
+    
+roota = Node(.2)
+roota.right = Node(.1)
+roota.left = Node(.1)
+vectora = [.3,.2,.2,.1,.2]
+print(nRound(vectora, roota))
 
 
  
